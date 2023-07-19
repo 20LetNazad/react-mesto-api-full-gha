32f82f930a066c0b06a7b41b32e58f61113ebc97
Daniil Davydovskiy <danildad.dad@yandex.ru>
2023-07-19 17:21:31 +0500

    fix: localhost

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const MainErrorHandler = require('./errors/MainErrorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 app.use(cors());
